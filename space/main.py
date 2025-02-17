f36800945bc3c40854952e12b89f4e065cd39a5f
Alvin <suhrobhotamov496@gmail.com>
2025-02-17 23:58:32 +0300

    Fix rocket's animation

--- space/main.py
+++ space/main.py
@@ -6,14 +6,24 @@ from modules.blink import blink
 from modules.fire import fire
 
 
+def init_file_contents():
+    file_contents = []
+    with open('modules/txt_frames/rocket_frame_1.txt', 'r', encoding='UTF-8') as first_file:
+        file_contents.append(first_file.read())
+    with open('modules/txt_frames/rocket_frame_2.txt', 'r', encoding='UTF-8') as second_file:
+        file_contents.append(second_file.read())
+    return file_contents
+
+
 def draw(canvas):
     canvas.border()
     canvas.nodelay(True)
     # curses.curs_set(False)
     max_rows, max_cols = canvas.getmaxyx()
+    file_contents = init_file_contents()
     coroutines = [
         fire(canvas=canvas, start_row=max_rows // 2, start_column=max_cols // 2),
-        animate_spaceship(canvas=canvas, start_row=(max_rows // 2) - 3, start_column=max_cols // 2),
+        animate_spaceship(canvas=canvas, start_row=(max_rows // 2) - 3, start_column=max_cols // 2, file_contents=file_contents),
     ]
     for col in range(1, max_cols-2):
         coroutines.append(blink(canvas=canvas, row=randint(1, max_rows-2), column=col))
--- space/modules/animate_spaceship.py
+++ space/modules/animate_spaceship.py
@@ -1,18 +1,16 @@
 import asyncio
+import time
 from itertools import cycle
 
 from modules.curses_tools import draw_frame, read_controls
 
 
-async def animate_spaceship(canvas, start_row, start_column):
-    file_contents = []
-    with open('modules/txt_frames/rocket_frame_1.txt', 'r', encoding='UTF-8') as first_file:
-        file_contents.append(first_file.read())
-    with open('modules/txt_frames/rocket_frame_2.txt', 'r', encoding='UTF-8') as second_file:
-        file_contents.append(second_file.read())
+async def animate_spaceship(canvas, start_row, start_column, file_contents):
     for step_text in cycle(file_contents):
-        print(step_text)
-        draw_frame(canvas, start_row, start_column, step_text, negative=False)
-        read_controls(canvas=canvas)
+        draw_frame(canvas, start_row, start_column, step_text)
         for _ in range(2):
+            rows_direction, columns_direction, space_pressed = read_controls(canvas=canvas)
+            start_row += rows_direction
+            start_column += columns_direction
             await asyncio.sleep(0)
+        canvas.refresh()
