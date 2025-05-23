c285e0330a6d6ebf0ef44bbeebfea9bef81ae1ba
hotamov <s.khotamov@yadro.com>
2025-05-23 21:31:38 +0300

    Добавлено круговое перемещение нескольких автобусов

--- async_bus_map_tracker/fake_bus.py
+++ async_bus_map_tracker/fake_bus.py
@@ -1,6 +1,8 @@
 import json
 import logging
 import os
+from itertools import chain, cycle, islice
+from random import randint
 from sys import stderr
 
 import trio
@@ -11,6 +13,10 @@ logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
 
 
+def generate_bus_id(route_id, bus_index):
+    return f"{route_id}-{bus_index}"
+
+
 def load_routes(directory_path='routes'):
     for filename in os.listdir(directory_path):
         if filename.endswith(".json"):
@@ -19,28 +25,32 @@ def load_routes(directory_path='routes'):
                 yield json.load(file)
 
 
-async def run_bus(url, bus_id, coordinates):
+async def run_bus(bus_index, bus_id, coordinates):
     try:
         async with open_websocket_url('ws://127.0.0.1:8001') as ws:
-            for coordinate in coordinates:
-                message = {
-                    "msgType": "Buses",
-                    "buses": [
-                        {"busId": bus_id, "lat": coordinate[0], "lng": coordinate[1], "route": bus_id},
-                    ]
-                }
-                await ws.send_message(json.dumps(message, ensure_ascii=True))
-                await trio.sleep(1)
-                logger.info(f'message send {message}')
+            while True:
+                for coordinate in islice(cycle(chain(coordinates, reversed(coordinates))), randint(10, 100)):
+                    message = {
+                        "msgType": "Buses",
+                        "buses": [
+                            {"busId": generate_bus_id(bus_id, bus_index), "lat": coordinate[0], "lng": coordinate[1], "route": bus_id},
+                        ]
+                    }
+                    await ws.send_message(json.dumps(message, ensure_ascii=True))
+                    await trio.sleep(0.01)
+                    logger.info(f'message send {message}')
     except OSError as ose:
         print('Connection attempt failed: %s' % ose, file=stderr)
+    except Exception as exc:
+        print(1)
 
 
 async def main():
     try:
         async with trio.open_nursery() as nursery:
             for route in load_routes():
-                nursery.start_soon(run_bus, '', route['name'], route['coordinates'])
+                for bus_index in range(randint(1, 10)):
+                    nursery.start_soon(run_bus, bus_index, route['name'], route['coordinates'])
     except ExceptionGroup as exc:
         print(exc)
 
